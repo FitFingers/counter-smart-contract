d91e6aa94891cea175da092ca5fb9c3825def8ea
FitFingers <hooper.j.1991@gmail.com>
2021-10-16 10:51:47 +0100

    feat: make counter very small

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -4,18 +4,33 @@ pragma solidity ^0.8.0;
 
 /**
  * @title Counter
- * Counter - The smallest smart contract I could develop
+ * Counter - A tiny counter contract for MainNet deployment experience
  */
 contract Counter {
+    uint256 public txCost = 0.000001 * 10**9 * 10**9;
     uint256 public count = 0;
+    address private owner;
 
-    constructor() {}
+    constructor() {
+        owner = address(msg.sender);
+    }
 
     function setCount(uint256 newCount) public payable {
+        require(msg.value >= txCost, "Not enough ETH");
         count = newCount;
     }
 
-    function getCount() public view returns (uint256) {
-        return count;
+    function increment() public {
+        count++;
+    }
+
+    function decrement() public {
+        count--;
+    }
+
+    function withdraw() public {
+        require(msg.sender == owner, "Only owner may withdraw");
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "There was a problem while withdrawing");
     }
 }
